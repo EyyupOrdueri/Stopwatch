bc230f8e9db55339349bffa4d5d2e093f6b231b2
eyyup <eyyupordueri@gmail.com>
2022-05-30 17:29:52 +0300

    File was updated.

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const start = document.getElementById('start');
 const reset = document.getElementById('reset');
 const timerDisplay = document.getElementById('timerDisplay');
 let millisecond = 0, second = 0, minute = 0, hour = 0;
+let loop;
 
 start.addEventListener('click', () => {
+    clearInterval(loop)
     loop = setInterval(counter, 10)
 })
 
@@ -15,7 +17,12 @@ pause.addEventListener('click', () => {
 reset.addEventListener('click', () => {
     clearInterval(loop);
     millisecond = 0, second = 0, minute = 0, hour = 0;
+    flag_cont.innerHTML = "0 : 0 : 0 : 0";
     timerDisplay.innerHTML = `${hour} : ${minute} : ${second} : ${millisecond}`;
+})
+
+flag.addEventListener('click', () => {
+    flag_cont.innerHTML = `${hour} : ${minute} : ${second} : ${millisecond}`;
 })
 
 function counter() {
